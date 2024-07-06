856e6018732284f5c76d07b821526957c6d22c12
ricocf <anandka64@gmail.com>
2024-07-06 22:25:12 +0200

    Fix Circle object

--- include/Circle.h
+++ include/Circle.h
@@ -10,10 +10,14 @@ template <class C>
 class Circle : public Shape<C>, public CountShape
 {
 private:
-    C radius;
+    std::shared_ptr<C> radius;
 
 public:
-    Circle(C rad) : radius(rad) { CountShape::count++; }
+    Circle(C rad)
+    {
+        radius = std::make_shared<C>(rad);
+        CountShape::count++;
+    }
     C area() const override;
     C perimeter() const override;
     void display() const override;
@@ -22,21 +26,21 @@ public:
 template <class C>
 C Circle<C>::area() const
 {
-    auto area_value = std::make_shared<double>((*radius) * (*radius) * (*pi));
+    auto area_value = ((*radius) * (*radius) * (*pi));
     return area_value;
 }
 
 template <class C>
 C Circle<C>::perimeter() const
 {
-    auto perimeter_value = std::make_shared<double>(2 * (*pi) * (*radius));
+    auto perimeter_value = (2 * (*pi) * (*radius));
     return perimeter_value;
 }
 
 template <class C>
 void Circle<C>::display() const
 {
-    std::cout << "Circle: radius = " << *radius << ", area = " << *area() << ", perimeter = " << *perimeter() << std::endl;
+    std::cout << "Circle: radius = " << *radius << ", area = " << area() << ", perimeter = " << perimeter() << std::endl;
 }
 
 #endif // CIRCLE_H
